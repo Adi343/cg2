7cedda94e01a80af59ce9bbc1d982d6e0a9708b3
Adi343 <53225414+Adi343@users.noreply.github.com>
2020-12-23 14:51:06 +0530

    c

--- backend/routes/postRoute.js
+++ backend/routes/postRoute.js
@@ -4,8 +4,14 @@ const post = require("../models/post");
 let router = express.Router();
 
 router.get("/", (req, res) => {
-  mongoose.Document
-  res.json({ message: "touche" });
+
+  post.find({}).exec().then(doc=>{
+    res.status(200).json(doc)
+  }).catch(err =>{
+    console.log(error)
+  });
+  // res.json({ message: "touche" });
+  res.json(post.find({}));
 });
 
 module.exports = router;
